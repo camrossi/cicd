fa49b46e1fb989a4424e31e128493546c2c8ef6d
Camillo <camrossi@cisco.com>
2019-11-27 15:55:41 +1100

    Configured vmware take 5

--- ACI/aci.tf
+++ ACI/aci.tf
@@ -59,7 +59,7 @@ resource "aci_application_epg" "admin" {
   application_profile_dn = "${aci_application_profile.app1.id}"
   name                   = "admin"
   relation_fv_rs_bd      = "${aci_bridge_domain.bd1.name}"
-  relation_fv_rs_dom_att = ["${data.aci_vmm_domain.vds.id}","${data.aci_phys_domain.Fab2.id}"]
+  relation_fv_rs_dom_att = ["${data.aci_vmm_domain.vds.id}"]
   relation_fv_rs_cons    = ["${aci_contract.contract_admin.name}"]
 }
 
