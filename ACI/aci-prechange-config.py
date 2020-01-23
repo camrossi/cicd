8f6fdf56556affa3d3d945f697af799bfee1a183
Camillo <camrossi@cisco.com>
2020-01-23 12:53:51 +1100

    NAE Test11

--- ACI/aci-prechange-config.py
+++ ACI/aci-prechange-config.py
@@ -22,7 +22,7 @@ topParentDn = topDn.getParent()
 topMo = md.lookupByDn(topParentDn)
 
 # build the request using cobra syntax
-fvBD = cobra.model.fv.BD(topMo, multiDstPktAct=u'bd-flood', mcastAllow=u'no', ipv6McastAllow=u'no', limitIpLearnToSubnets=u'yes', unicastRoute=u'yes', unkMcastAct=u'flood', v6unkMcastAct=u'flood', descr=u'', hostBasedRouting=u'no', llAddr=u'::', nameAlias=u'', type=u'regular', ipLearning=u'yes', vmac=u'not-applicable', mac=u'00:22:BD:F8:19:FF', epMoveDetectMode=u'', ownerTag=u'', intersiteBumTrafficAllow=u'no', annotation=u'', ownerKey=u'', name=u'BD1', epClear=u'no', unkMacUcastAct=u'proxy', arpFlood=u'no', intersiteL2Stretch=u'no', OptimizeWanBandwidth=u'no')
+fvBD = cobra.model.fv.BD(topMo, multiDstPktAct=u'bd-flood', mcastAllow=u'no', ipv6McastAllow=u'no', limitIpLearnToSubnets=u'yes', unicastRoute=u'yes', unkMcastAct=u'flood', v6unkMcastAct=u'flood', descr=u'', hostBasedRouting=u'no', llAddr=u'::', nameAlias=u'', type=u'regular', ipLearning=u'yes', vmac=u'not-applicable', mac=u'00:22:BD:F8:19:FF', epMoveDetectMode=u'', ownerTag=u'', intersiteBumTrafficAllow=u'no', annotation=u'', ownerKey=u'', name=u'BD2', epClear=u'no', unkMacUcastAct=u'proxy', arpFlood=u'no', intersiteL2Stretch=u'no', OptimizeWanBandwidth=u'no')
 fvSubnet = cobra.model.fv.Subnet(fvBD, name=u'', descr=u'', ctrl=u'', ip=u'2.0.0.1/8', preferred=u'no', virtual=u'no', nameAlias=u'', annotation=u'')
 fvRsMldsn = cobra.model.fv.RsMldsn(fvBD, tnMldSnoopPolName=u'', annotation=u'')
 fvRsIgmpsn = cobra.model.fv.RsIgmpsn(fvBD, tnIgmpSnoopPolName=u'', annotation=u'')
@@ -32,6 +32,7 @@ fvRsBDToNdP = cobra.model.fv.RsBDToNdP(fvBD, annotation=u'', tnNdIfPolName=u'')
 
 
 # commit the generated code to APIC
+print(toXMLStr(topMo))
 c = cobra.mit.request.ConfigRequest()
 c.addMo(topMo)
 md.commit(c)
